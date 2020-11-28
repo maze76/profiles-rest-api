f46d1450343cbb68d2df9caa53e0fafea82c330f
maze76 <zeljkomarcinko@gmail.com>
2020-11-28 21:18:03 +0100

    Added hello api view

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'profiles_api'
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +52,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'profiles_project.urls'
+
 
 TEMPLATES = [
     {
